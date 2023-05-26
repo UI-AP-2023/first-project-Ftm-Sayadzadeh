9645dbcf28352cb79a2921892a25f741838b3c91
Ftm-Sayadzadeh <ftmdkf@gmail.com>
2023-05-27 01:29:06 +0430

    - Some Edited on mainPage.fxml + Added images.

--- src/main/java/com/example/onlineshop/MainPageGraphicController.java
+++ src/main/java/com/example/onlineshop/MainPageGraphicController.java
@@ -1,12 +1,16 @@
 package com.example.onlineshop;
 
+import com.example.onlineshop.model.products.Pen;
+import com.example.onlineshop.model.products.Product;
 import com.example.onlineshop.model.user.Customer;
-import com.example.onlineshop.view.LogInPage;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import com.example.onlineshop.model.processes.* ;
+
+import java.util.ArrayList;
 
 public class MainPageGraphicController {
     @FXML
@@ -15,6 +19,14 @@ public class MainPageGraphicController {
 
     @FXML
     void accountIcon(MouseEvent event) throws Exception {
+        MainPageGraphicController.customer = new Customer("ala" , "a@email.com" , "09137900843" , "12345678");
+        Pen pen1 = new Pen("pen1",1000,1,"iran","Black");
+        Pen pen2 = new Pen("pen2",2000,1,"iran","Black");
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(pen1);
+        products.add(pen2);
+        Receipt receipt = new Receipt(products);
+        MainPageGraphicController.customer.getShoppingHistory().add(receipt);
         if(customer == null){
             new SignUpAndLogInPage().start((Stage) ((Node) event.getSource()).getScene().getWindow());
         }
